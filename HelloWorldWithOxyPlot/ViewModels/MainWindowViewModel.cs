8f1aa9e04716e28e1238ff4798bc6798f9e39cc0
Eric A. Livesay <e_livesay@hotmail.com>
2018-11-06 17:04:54 -0800

    Actually plotting data , specified in the ViewModel,  in the View. See http://docs.oxyplot.org/en/latest/getting-started/hello-wpf-xaml.html

--- HelloWorldWithOxyPlot/ViewModels/MainWindowViewModel.cs
+++ HelloWorldWithOxyPlot/ViewModels/MainWindowViewModel.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OxyPlot;
 
-namespace HelloWorldWithOxyPlot.ViewModels
+//namespace HelloWorldWithOxyPlot.ViewModels
+namespace HelloWorldWithOxyPlot
 {
    public class MainWindowViewModel
    {
+      public string Title { get; private set; }
+      public IList<DataPoint> Points { get; private set; }
+
+      public MainWindowViewModel()
+      {
+         this.Title = "Example 2";
+         this.Points = new List<DataPoint>
+         {
+            new DataPoint(0,4),
+            new DataPoint(10,13),
+            new DataPoint(20,15),
+            new DataPoint(30,16),
+            new DataPoint(40,12),
+            new DataPoint(50,12),
+         };
+
+
+      }
 
    }
 }
